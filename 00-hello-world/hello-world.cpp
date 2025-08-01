60846f9a97bfc9cfc9a49c547b6dc7196a1dc1c6
Satyajit Samal <samalsatyajit668@gmail.com>
2025-08-01 08:30:38 +0530

    add hello-world

--- 00-hello-world/hello-world.cpp
+++ 00-hello-world/hello-world.cpp
@@ -1,9 +1,22 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     string name = "Satyajit";
+//     int age = 23;
+
+//     cout << name << " is " << age << " years old. " << endl;
+//     return 0;
+// }
+
+
 #include <iostream>
-using namespace std;
 
 int main() {
-    string name = "Satyajit";
-    int age = 23;
-    cout << name << " is " << age << " years old.";
+    std::string name1 = "Satyajitt";
+    int age1 = 23;
+
+    std::cout << name1 << " is " << age1 << " years old." << std::endl;
+    std::cout << 3 * 3;
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,20 @@
+# C++ From Scratch 🚀
+
+This repository contains my daily learning journey through C++ using W3Schools as a structured guide.  
+All code is written, tested, and committed from scratch — without copying directly.
+
+## Topics Covered
+- Syntax & structure
+- Variables & data types
+- Input/Output
+- Conditions & loops
+- Arrays & strings
+- Functions
+- Pointers
+- Object-Oriented Programming
+
+## Why this repo?
+✅ Build coding habit  
+✅ Understand core C++ concepts  
+✅ Prepare for DSA, coding rounds, or interviews  
+✅ Track progress publicly
